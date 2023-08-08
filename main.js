b32dadbc700ba90c49bad32505f883db8e24a753
hibah11 <hboba-2012@hotmail.com>
2023-08-09 00:29:17 +0300

    n

--- main.js
+++ main.js
@@ -60,11 +60,57 @@ function returnBook(title) {
           books[i].availablity = true;
           console.log(title + " is avaliable now");
         } else {
-          console.log(title + "was never borrowed");
+          console.log(title + " was never borrowed");
         }
         return;
       }
     }
   }
-returnBook("air");
+returnBook("north");
 
+/* 4 */
+/* done testing */
+
+
+/* 5 */
+
+for ( i = 0; i < books.length; i++) {
+    console.log(books[i].title);
+    console.log(books[i].author);
+    console.log(books[i].npages);
+    if (books.borrower) {
+        console.log(`Borrower: ${book.borrower}`);
+      }
+  }
+
+/* 5 */
+
+function addBook(newBook) {
+    books.push(newBook);
+  }
+
+  let newBook = {
+    title: "The book",
+    author: "Jj",
+    npages: 222,
+    availablity : true
+  };
+
+/* 6 */
+
+function searchBook(searchTerm) {
+    searchTerm = searchTerm.toLowerCase();
+  
+    return books.filter(book => {
+      const { title, author, borrower } = book;
+      return (
+        title.toLowerCase().includes(searchTerm) ||
+        author.toLowerCase().includes(searchTerm) ||
+        (borrower && borrower.toLowerCase().includes(searchTerm))
+      );
+    });
+  }
+
+  let searchTerm1 = "adam";
+  let results1 = searchBook(searchTerm1);
+  console.log(results1);
